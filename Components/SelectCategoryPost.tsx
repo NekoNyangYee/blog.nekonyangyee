e17e36309e056306ff8742e194dfd202b7ccc430
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2024-09-07 23:57:43 +0900

    fix: 롤백 2

--- Components/SelectCategoryPost.tsx
+++ Components/SelectCategoryPost.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import { Post, allPosts } from "contentlayer/generated";
 import { format } from "date-fns";
@@ -10,17 +8,15 @@ interface SelectCategoryPostProps {
 }
 
 const SelectCategoryPost = ({ category }: SelectCategoryPostProps) => {
-  const categoryPosts: Post[] = allPosts
-    .filter((post) => post.category === category)
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  const categoryPosts: Post[] = allPosts.filter((post) => post.category === category);
 
   return (
     <StyledSelectCategoryPost>
       <h1>"{category}"의 다른 게시물</h1>
       <div className="selectCategory-post-container">
         {categoryPosts.map((post) => (
-          <Link href={`/${post._raw.flattenedPath}`} key={post._id}>
-            <div className="category-post-container">
+          <Link href={`/${post._raw.flattenedPath}`}>
+            <div key={post._id} className="category-post-container">
               <img src={post.teaser === null ? "/no-image.png" : post.teaser} alt={post.title} />
               <h2>{post.title}</h2>
               <time dateTime={post.date}>{format(new Date(post.date), "yyyy년 MM월 dd일")}</time>
